2f3e06641345d991c162d8f4b86400d14a2877d2
Alexander Borel <alex.b.minsk@gmail.com>
2017-01-28 18:16:47 +0300

    1.0

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,9 +1,25 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+      this.length = 0;
+      this._head = null;
+      this._tail = null;
+    }
 
-    append(data) {}
+    append(data) {
+      var node = new Node(data);
+      if (this.length === 0) {
+        this._head = node;
+        this._tail = node;
+      } else {
+        this._tail.next = node;
+        node.prev = this._tail;
+        this._tail = node;
+      }
+      this.length++;
+      return this;
+    }
 
     head() {}
 
