895283525c82ddcf0893d81f966e057b7d3fa3af
Alexander Borel <alex.b.minsk@gmail.com>
2017-01-28 19:11:00 +0300

    3.0

--- src/linked-list.js
+++ src/linked-list.js
@@ -29,19 +29,72 @@ class LinkedList {
       return this._tail.data;
     }
 
-    at(index) {}
+    at(index) {
+      var count = 0;
+      var currentNode = this._head;
+      while ( count < index ) {
+        currentNode = currentNode.next;
+        count++;
+      }
+      return currentNode.data;
+    }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+      var count = 0;
+      var currentNode = this._head;
+      while (currentNode) {
+        if ( count === index) {
+          currentNode.data = data;
+        }
+          currentNode = currentNode.next;
+          count++;
+      }
+      return this;
+    }
 
-    isEmpty() {}
+    isEmpty() {
+      if ( this.length === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
 
-    clear() {}
+    clear() {
+      this.length = 0;
+      this._head = null;
+      this._tail = null;
+      return this;
+    }
 
     deleteAt(index) {}
 
-    reverse() {}
+    reverse() {
+      var node_head = this._head;
+      var node_tail = this._tail;
+      var i = 0;
+      var temp;
+      while ( i < Math.floor( this.length / 2)) {
+        temp = node_tail.data;
+        node_tail.data = node_head.data;
+        node_head.data = temp;
+        node_head = node_head.next;
+        node_tail = node_tail.prev;
+        i++;
+      }
+      return this;
+    }
 
-    indexOf(data) {}
+    indexOf(data) {
+      var node = this._head;
+      for ( var i = 0; i < this.length; i++) {
+        if (node.data === data) {
+          return i;
+        }
+        node = node.next;
+      }
+      return -1;
+    }
 }
 
 module.exports = LinkedList;
