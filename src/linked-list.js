5f0fa13395c44ed4e919b7851610af8c154870dd
Alexander Borel <alex.b.minsk@gmail.com>
2017-02-02 15:51:16 +0300

    doubly-linked-list: all methods completed

--- src/linked-list.js
+++ src/linked-list.js
@@ -2,98 +2,129 @@ const Node = require('./node');
 
 class LinkedList {
     constructor() {
-      this.length = 0;
-      this._head = null;
-      this._tail = null;
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
     }
 
     append(data) {
-      var node = new Node(data);
-      if (this.length === 0) {
-        this._head = node;
-        this._tail = node;
-      } else {
-        this._tail.next = node;
-        node.prev = this._tail;
-        this._tail = node;
-      }
-      this.length++;
-      return this;
+        var node = new Node(data);
+        if (this.length === 0) {
+            this._head = node;
+            this._tail = node;
+        } else {
+            this._tail.next = node;
+            node.prev = this._tail;
+            this._tail = node;
+        }
+        this.length++;
+        return this;
     }
 
     head() {
-      return this._head.data;
+        if (this._head !== null) {
+            return this._head.data;
+        } else {
+            return null;
+        }
     }
 
     tail() {
-      return this._tail.data;
+        if (this._tail !== null) {
+            return this._tail.data;
+        } else {
+            return null;
+        }
     }
 
     at(index) {
-      var count = 0;
-      var currentNode = this._head;
-      while ( count < index ) {
-        currentNode = currentNode.next;
-        count++;
-      }
-      return currentNode.data;
+        var count = 0;
+        var currentNode = this._head;
+        while (count < index) {
+            if (currentNode.next !== null) {
+                currentNode = currentNode.next;
+                count++;
+            } else {
+                throw console.error("Index out of range");
+            }
+        }
+        return currentNode.data;
     }
 
     insertAt(index, data) {
-      var count = 0;
-      var currentNode = this._head;
-      while (currentNode) {
-        if ( count === index) {
-          currentNode.data = data;
+        var count = 0;
+        var currentNode = this._head;
+        while (currentNode) {
+            if (count === index) {
+                currentNode.data = data;
+            }
+            currentNode = currentNode.next;
+            count++;
         }
-          currentNode = currentNode.next;
-          count++;
-      }
-      return this;
+        return this;
     }
 
     isEmpty() {
-      if ( this.length === 0) {
-        return true;
-      } else {
-        return false;
-      }
+        if (this.length === 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     clear() {
-      this.length = 0;
-      this._head = null;
-      this._tail = null;
-      return this;
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
+        return this;
     }
 
-    deleteAt(index) {}
+    deleteAt(index) {
+        var counter = 0;
+        var currentNode = this._head;
+        while (currentNode) {
+            if (counter === index) {
+                if (currentNode.prev !== null) {
+                    currentNode.prev.next = currentNode.next;
+                }
+                if (currentNode.next !== null) {
+                    currentNode.next.prev = currentNode.prev;
+                }
+                this.length--;
+                break;
+            }
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return this;
+    }
 
     reverse() {
-      var node_head = this._head;
-      var node_tail = this._tail;
-      var i = 0;
-      var temp;
-      while ( i < Math.floor( this.length / 2)) {
-        temp = node_tail.data;
-        node_tail.data = node_head.data;
-        node_head.data = temp;
-        node_head = node_head.next;
-        node_tail = node_tail.prev;
-        i++;
-      }
-      return this;
+        var node = this._head;
+        var temp = null;
+        while (node !== null) {
+            temp = node.prev;
+            node.prev = node.next;
+            node.next = temp;
+            node = node.prev;
+        }
+        temp = this._head;
+        this._head = this._tail;
+        this._tail = temp;
+        return this;
     }
 
     indexOf(data) {
-      var node = this._head;
-      for ( var i = 0; i < this.length; i++) {
-        if (node.data === data) {
-          return i;
+        var node = this._head;
+        var count = 0;
+        while (node) {
+            if (node.data === data) {
+                return count;
+            }
+            node = node.next;
+            count++;
         }
-        node = node.next;
-      }
-      return -1;
+        return -1;
     }
 }
 
